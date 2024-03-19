214a225ecc23406656de67b02b1c3c1556b7fda6
Souvik Dey <shoredasherdey680@gmail.com>
2024-03-19 12:17:53 +0530

    modified:   src/components/Cart/CartItem.module.css 	modified:   src/components/Layout/Header.js 	modified:   src/components/Layout/Header.module.css 	modified:   src/components/Layout/HeaderCartButton.js 	modified:   src/components/Layout/HeaderCartButton.module.css 	modified:   src/components/Meals/AvailableMeals.js 	modified:   src/components/Meals/MealsSummary.module.css 	modified:   src/index.css

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,27 +5,27 @@ import classes from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
+    name: 'Fish meal',
     description: 'Finest fish and veggies',
-    price: 22.99,
+    price: 220.99,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
+    name: 'Egg meal',
+    description: 'An indian specialty!',
+    price: 160.5,
   },
   {
     id: 'm3',
     name: 'Barbecue Burger',
     description: 'American, raw, meaty',
-    price: 12.99,
+    price: 120.99,
   },
   {
     id: 'm4',
     name: 'Green Bowl',
     description: 'Healthy...and green...',
-    price: 18.99,
+    price: 180.99,
   },
 ];
 
